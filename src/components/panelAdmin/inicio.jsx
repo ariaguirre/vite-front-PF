8ecfcfab76618e075052d4c29f4df79a94d06823
Bianca Stork <biancastrk@gmail.com>
2023-05-01 04:10:07 -0300

    Actualizacion Vistas Componentes Panel Admin

--- src/components/panelAdmin/inicio.jsx
+++ src/components/panelAdmin/inicio.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Divider } from '@mui/material'
+
 
 const Inicio = () => {
   return (
-    <Box component="main" sx={{ flexGrow: 1, p: 3, alignContent:"center"}}>
+    <div>
+    <Box component="main"
+    sx={{ width: '50%',alignContent:"center", display: 'flex',
+    justifyContent: 'center', mx:"25%", mt:"2%"}}
+     textAlign={"center"}
+     boxShadow={3}
+     alignContent={"center"}
+     bgcolor={"primary"} 
+     >
     <h1>
      BIENVENIDOS AL PANEL DE ADMINISTRADOR !
     </h1>
     </Box>
+   
+    </div>
   )
 }
 
