121cbd982d0f644e1e702826822aada2ec726811
Silvana Fernandez <silvanafernandezar@gmail.com>
2023-04-30 22:31:00 -0300

    formulario creado para agregar productos

--- src/components/panelAdmin/createProduct.jsx
+++ src/components/panelAdmin/createProduct.jsx
@@ -1,53 +1,113 @@
-import { Box, Divider, FormControl, Toolbar } from '@mui/material'
-import React from 'react'
+import { Box, Divider, FormControl, Toolbar } from '@mui/material';
+import {useState, useEffect} from 'react';
+import { postProductsAdmin, getCategories } from '../../utils/firebase/firebaseClient';
+import { getCategoriesAction } from '../../features/categories/categoriesSlice';
+import Images from '../images/images'
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useDispatch, useSelector } from 'react-redux';
 
 const createProduct = () => {
 
-  const [category, setCategory] = React.useState('');
+  
+  const [urlImages, setUrlImages] = useState([]);
+  const dispatch = useDispatch()
 
-  const handleChange = (event) => {
-    setCategory(event.target.value);
+  const dataCategories = useSelector(state => state.categories)
+
+  const [product, setProduct] = useState({
+    name: '',
+    description: '',
+    stock: 0,
+    price: 0,
+    categories: [],
+    imageUrl: [],
+    reviews: [],
+    rating: 0,
+    sale: {}
+    
+    
+});
+
+  const handleChange =  (event) => {
+    const property = event.target.name;
+    const value = event.target.value;
+    setProduct({
+        ...product,
+        [property] : value
+    })
+}
+
+const handleSelect =(event) => {
+  setProduct({
+      ...product,
+      categories: [{[event.target.value]: true}]
+  })
+}
+
+ useEffect(() => {
+  const getCat = async() => {
+    const result = await getCategories()
+    dispatch(getCategoriesAction(result))
   };
+  getCat()
+}, [])
+
+ 
+
+useEffect(()=>{
+  if(urlImages.length < 1) return;
+  setProduct({
+    ...product,
+    imageUrl: [...product.imageUrl, urlImages]
+  })
+}, [urlImages])
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = product
+  try{
+  await postProductsAdmin(data)
+    console.log(product.name)
+  } catch(error){
+    console.log('No funciona')
+  }
+}
+
 
   return (
     
-    <Box 
-      component="form"
-      sx={{ flexGrow: 1, p: 3, alignContent:"center"}}
-      noValidate
-      autoComplete="on"
-      padding="15px"
-    > <h1>NUEVO PRODUCTO</h1>  
-   
-      <TextField id="outlined-basic" label="Nombre Producto" variant="outlined" />
-      <TextField id="outlined-basic" label="url Imagen" variant="outlined" padding= "2em"/>
-      <Divider />
-      <h1>Categorias</h1>
-      <FormControlLabel control={<Checkbox/>} label="Andaderas" />
-      <FormControlLabel control={<Checkbox/>} label="Andaderas y Correpasillos" />
-      <FormControlLabel control={<Checkbox/>} label="Correpasillos" />
-     <br></br>
-      <TextField id="outlined-basic" label="url Imagen" variant="outlined" />
-      <TextField id="outlined-basic" label="Precio" variant="outlined" />
-      <br></br>
-      <Button type='submit' variant='contained' margin='dense' >Crear Producto</Button>
-  
-    </Box>
+    <form onSubmit={handleSubmit}>
 
-   
-    
-
-   
-      
-   
+            <label htmlFor="">Name </label>
+            <input type="text" name="name" value={product.name} placeholder="name" onChange={handleChange}/>
+            <br />
+            <label htmlFor="">Description</label>
+            <textarea type="text" name="description" value={product.description} placeholder="description" onChange={handleChange}></textarea>
+            <br />
+            <label htmlFor="">Stock</label>
+            <input type="number" name="stock" value={product.stock} placeholder="stock" onChange={handleChange}/>
+            <br />
+            <label htmlFor="">Price</label>
+            <input type="text" name="price" placeholder="price" value={product.price} onChange={handleChange} />
+            <br />
+            <label htmlFor="">Categories</label>
+            <select onChange={handleSelect}>
+                {dataCategories?.categories?.map((category, index)=> (
+                    <option key={index} value={category.id}>{category.id}</option>
+                ))}
+            </select>
+            <br />
+            <label htmlFor="">Image</label>
+            <Images setUrlImages={setUrlImages}/>
+            <br />
+            <button type="submit">Enviar</button>
+           
+        </form>
   );
 
-   
-  
 }
 
 export default createProduct
