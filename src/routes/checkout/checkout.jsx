1939c65a0870edcae0e41d3dc525706e5beebb33
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-05-04 16:22:44 -0500

    creacion de la pagina shop

---
+++ src/routes/checkout/checkout.jsx
@@ -0,0 +1,10 @@
+import Container from '@mui/material/Container'
+const Checkout = () => {
+  return (
+    <Container maxWidth="xl" sx={{minHeight:"100vh"}}>
+      
+    </Container>
+  )
+}
+
+export default Checkout
