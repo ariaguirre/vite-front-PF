45acfcd11d41b597f5bd962de675ebf8798db245
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-05-04 21:17:00 -0500

    cart terminado

--- src/routes/checkout/checkout.jsx
+++ src/routes/checkout/checkout.jsx
@@ -1,14 +1,21 @@
 import { useSelector } from 'react-redux'
-
 import styles from "./checkout.module.css"
 import CheckoutItem from '../../components/checkout-item/checkout-item';
 import { numberFormat } from '../../helper/numberFormat';
-
+import { useEffect, useState } from 'react';
 
 const Checkout = () => {
 
   const cartItems = useSelector(state => state.cart.cartItems);
-  
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
+    setTotal(newCartTotal);
+  }, [cartItems])
+
+
+
   return (
     <div className={styles.checkoutContainer}>
       <div className={styles.checkoutHeader}>
@@ -32,7 +39,7 @@ const Checkout = () => {
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <span className={styles.total}>Total: {numberFormat(1000000)}</span>
+      <span className={styles.total}>Total: {numberFormat(total)}</span>
     </div>
 
   )
