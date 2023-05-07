d7f1544ab2dfe4890d80d8b09014e882e6b05cbc
Bianca Stork <biancastrk@gmail.com>
2023-05-07 12:25:23 -0300

    cambios de estilos en Boton y Titulo Hero - banner funcionando

--- src/components/hero-section/slider/slider.jsx
+++ src/components/hero-section/slider/slider.jsx
@@ -1,36 +1,38 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react';
 import { Box, Button, Typography } from '@mui/material'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const Slider = () => {
-const textBanner =["LA MATERNIDAD ES UN CAMINO QUE TE ENSEÑA A SER PERSEVERANTE, POSITIVA, RECURSIVA Y MAGICA", "CLARO QUE HAY DIAS O ETAPAS DIFICILES Y ESO TAMBIEN ES NORMAL"];
+const textBanner =["LA MATERNIDAD ES UN CAMINO QUE TE ENSEÑA A SER PERSEVERANTE, POSITIVA, RECURSIVA Y MAGICA","CLARO QUE HAY DIAS O ETAPAS DIFICILES Y ESO TAMBIEN ES NORMAL", "DIOS TE ELIGIÓ PARA SER MADRE EN EL MOMENTO JUSTO", "Y TE DOTO DE TODO LO QUE NECESITAS PARA SER LA MAMÁ PERFECTA PARA TU BEBE"];
+
+const [index, setIndex] = useState(0);
+useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((i) => (i + 1) % textBanner.length);     
+    }, 5000);
+    return () => clearInterval(interval);
+ }, []);
+
 
 return (
   <>
   <Box
+   
    sx={{
    width: "100%",
-   height: 250,
-   backgroundColor: 'primary.main',
+   height: 110,
+   //**CAMBIE COLOR BACKGROUND BANNER */
+   //background: "#0d47a1",
+   background:"#1976d2",
+  
   }}
   >
-  <Typography  color={"secondary"} padding={1} pt={5} variant='h5'>
-  {textBanner[0]} </Typography> 
+  <Typography  color={"secondary"} padding={0} pt={3} variant='h6' >
+  {textBanner[index]} </Typography> 
   <FavoriteBorderIcon color={"secondary"}/>
-  <br />
-  <Typography  color={"secondary"} padding={2}  pt={5} variant='h7body1'>
-  {textBanner[1]}
-  </Typography>
- <Box padding={5}>
- <Button  color="secondary" ><ArrowBackIosNewIcon /></Button>
-  <Button color="secondary"><ArrowForwardIosIcon /></Button>  
-  </Box>
- 
-  <Button  color="secondary"><ArrowBackIosNewIcon /></Button>
-  <Button color="secondary"><ArrowForwardIosIcon /></Button>     
   </Box> 
   </>
   )
