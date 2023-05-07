47785501c705bd264a1ad0c5b3e62390f695a063
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-05-07 13:11:48 -0500

    listo conexion detail

--- src/routes/detail/ProductDetail.jsx
+++ src/routes/detail/ProductDetail.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Container from '@mui/material/Container'
 import Stack from '@mui/material/Stack';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
@@ -10,55 +11,76 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import {getProductId } from '../../features/productsId/productsIdSlice';
 import HalfRating from '../../components/card/rating/rating';
+import { addItemToCart } from '../../features/cartSlice/cartSlice';
 
 
 const DetailProduct = () => {
 
   const { id } = useParams()
   const dispatch = useDispatch();
-  const p = useSelector((state) => state.productsId);
-  let detail = p.productsId[0];
-  console.log(detail)
+  const {productsId} = useSelector((state) => state.productsId);
 
   useEffect(() =>{
-    const unit = async () =>{
+  (async function unit(){
       const result = await getProductByid(id)
       dispatch(getProductId(result))
-    }
-    unit()
-  },[dispatch])
+    })()
+  },[])
   
+  var cantidad = [];
+
+  const handleCant = (e) =>{
+    cantidad.push(e.target.value)
+  }
+
+
+  const handleClickCartIcon = () => {
+    const product = {
+      id:productsId[0]?.id,
+      title:productsId[0]?.name,
+      imageUrl:productsId[0]?.imageUrl,
+      price:productsId[0]?.price,    
+    }
+    
+    for(let i = 0; i<cantidad[0]; i++){
+      dispatch(addItemToCart(product));
+    }
+  }
+
+
 
 
   return <>
-    <Container fixed className="detail">
+    <Container fixed>
       <Stack direction={{ xs: 'column', sm: 'row' }} alignItems="center" spacing={2} justifyContent="center" sx={{ px: "2rem", py: "2rem" }}>
         <Box sx={{maxWidth:'50%' }}>
-          <img src={detail.imageUrl} className=" imgDetail" alt={detail.name} />
+          <img src={productsId[0]?.imageUrl} alt={productsId[0]?.name} />
 
         </Box>
 
-        <div className=" infDetail">
+        <div>
           <form>
             <Typography variant="h3">
-              {detail.name}
+              {productsId[0]?.name}
             </Typography>
 
             <hr />
 
-            <div className="infoInicial">
-              <p>precio:<span className='money' color='green'>$</span><span className='price'>{detail.price}</span></p>
+            <div>
+              <p>precio:<span>$</span><span>
+                {productsId[0]?.price}
+                </span></p>
               <Stack>
                 <p>estrellas:</p>
-                <HalfRating rValue={detail.rating} />
+                <HalfRating  />
               </Stack>
-              <p>Unidades disponibles en stock:<span>{detail.stock}</span></p>
+              <p>Unidades disponibles en stock:<span>{productsId[0]?.stock}</span></p>
               <Stack direction="row" >
                 <p>Cantidad:</p>
-                <input type="number" min='0' className="cant" />
+                <input type="number" min='0' onChange={(e) =>handleCant(e)} value={1} className="cant" />
               </Stack>
               <Stack direction="row" alignContent={'center'} gap={2} >
-                <Button variant="contained" sx={{ mt: 2, mb: 2 }} startIcon={<AddIcon />}>Agregar al carrito</Button>
+                <Button variant="contained" sx={{ mt: 2, mb: 2 }} onClick={handleClickCartIcon} startIcon={<AddIcon />}>Agregar al carrito</Button>
                 <Link to="/shop/checkout">
                   <Button variant="contained" sx={{ mt: 2, mb: 2 }}  startIcon={<AddShoppingCartIcon />}>Ir al carrito</Button>
                 </Link>
@@ -80,8 +102,12 @@ const DetailProduct = () => {
         <Typography variant='h3' fontSize={30} fontWeight={500}>
           Reseñas del producto
         </Typography>
+        <Box>
+          {
+            productsId[0]?.reviews.length < 1 ? <span>No hay comentarios</span> : productsId[0]?.reviews[0] 
 
-
+          }
+        </Box>
             
       </Box>
 
