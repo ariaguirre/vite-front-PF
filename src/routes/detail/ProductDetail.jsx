232d90136e01e01f5e17f2ee851d9df76aa26923
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-05-08 00:52:06 -0500

    avance en los estilos del detail

--- src/routes/detail/ProductDetail.jsx
+++ src/routes/detail/ProductDetail.jsx
@@ -1,18 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import Container from '@mui/material/Container'
-import Stack from '@mui/material/Stack';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import AddIcon from '@mui/icons-material/Add';
 import { Link, useParams } from 'react-router-dom'
-import { Box, Typography, Button } from '@mui/material';
+import { Button } from '@mui/material';
 //redux
 import { getProductByid } from '../../utils/firebase/firebaseClient';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import {getProductId } from '../../features/productsId/productsIdSlice';
 import HalfRating from '../../components/card/rating/rating';
-import CheckoutItem from '../../components/checkout-item/checkout-item';
-import { addItemToCart, clearItemFromCart, deleteCartItem } from '../../features/cartSlice/cartSlice';
+import styles from "./productDetail.module.css";
+import { addItemToCart,  deleteCartItem } from '../../features/cartSlice/cartSlice';
 
 
 const DetailProduct = () => {
@@ -51,75 +49,72 @@ const DetailProduct = () => {
 
 
 
+  const newLocal = (styles.contenedor, styles.infBaseDetail);
+
+  
   return <>
-    <Container fixed>
-      <Stack direction={{ xs: 'column', sm: 'row' }} alignItems="center" spacing={2} justifyContent="center" sx={{ px: "2rem", py: "2rem" }}>
-        <Box sx={{maxWidth:'50%' }}>
+    <div className={styles.contenedor}>
+      <div className={styles.detailTop}>
+        
+        <div className={styles.imgProduct}>
           <img src={productsId[0]?.imageUrl} alt={productsId[0]?.name} />
+        </div>
 
-        </Box>
-
-        <div>
+        <div className={styles.infBaseDetail}>
           <form>
-            <Typography variant="h3">
-              {productsId[0]?.name}
-            </Typography>
+            <h2 className={styles.titleDetail}>{productsId[0]?.name}</h2>
 
-            <hr />
+            <hr className={styles.division} />
 
-            <div>
-              <p>precio:<span>$</span><span>
-                {productsId[0]?.price}
-                </span></p>
-              <Stack>
-                <p>estrellas:</p>
+            <div className={styles.infBaseDetail}>
+              <p className={styles.price}><span className={styles.money}>$</span>{productsId[0]?.price}</p>
+              <div className={styles.rating}>
+                <span>Calificación:</span>
                 <HalfRating  />
-              </Stack>
-              <p>Unidades disponibles en stock:<span>{productsId[0]?.stock}</span></p>
-              <Stack direction="row" >
-                <p>Cantidad:</p>
-                  <div>
+              </div>
+              <p><span>Unidades disponibles:</span>{productsId[0]?.stock}</p>
+              <div className={styles.flex} >
+                <span>Cantidad:</span>
+                  <div className={styles.flex}>
                   <div onClick={()=> removeItemHandler()}> &#10094;</div>
                   <span >{quantity}</span>        
                   <div   onClick={() => addItemHandler()}>&#10095;</div>
                 </div>
-              </Stack>
-              <Stack direction="row" alignContent={'center'} gap={2} >
-                <Button variant="contained" sx={{ mt: 2, mb: 2 }} onClick={handleClickCartIcon} startIcon={<AddIcon />}>Agregar al carrito</Button>
+              </div>
+              <div className={styles.detailBuy} >
+                <Button variant="contained"  onClick={handleClickCartIcon} startIcon={<AddIcon />}>Agregar al carrito</Button>
                 <Link to="/shop/checkout">
-                  <Button variant="contained" sx={{ mt: 2, mb: 2 }}  startIcon={<AddShoppingCartIcon />}>Ir al carrito</Button>
+                  <Button variant="contained" className={styles.btn} startIcon={<AddShoppingCartIcon />}>Ir al carrito</Button>
                 </Link>
-              </Stack>
-
+              </div>
             </div>
-            <hr />
-            <div>
+
+            <hr className={styles.division}  />
+            <div className={styles.infBaseDetail}>
               <h3>Tipos de entrega</h3>
-              <p>Retiro en tienda:</p>
-              <p>Disponible para despacho</p>
+              <p><span>Retiro en tienda:</span></p>
+              <p><span>Disponible para despacho</span></p>
             </div>
           </form>
         </div>
 
-      </Stack>
+      </div>
 
-      <Box p={4} borderRadius={8} bgcolor={'#f4f4f4'} mb={2}>
-        <Typography variant='h3' fontSize={30} fontWeight={500}>
-          Reseñas del producto
-        </Typography>
-        <Stack sx={{pl:"2rem"}}>
+      <div className={newLocal}>
+        <h3 className={styles.titleDetail}>Reseñas del producto</h3>
+        {/* <Stack sx={{pl:"2rem"}}>
           {productsId[0]?.length < 1 ? <p>No hay reseñas</p> : productsId[0]?.reviews.map((r, index) =>{ return (<Box key={index}>
             <p>Usuario:{r.user}</p>
             <p>Reseña:{r.review}</p>
             <p>Fecha de la reseña:{r.date}</p>
 
           </Box>)}) }
-        </Stack>
+        </Stack> */}
             
-      </Box>
+      </div>
 
 
-    </Container>
+    </div>
   </>
 }
 
