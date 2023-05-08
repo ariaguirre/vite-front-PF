42d5ee5b9a9421a8cf691b1ec4f754c6a4532439
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-05-08 10:43:34 -0500

    cambios para el merge

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -3,7 +3,6 @@ import CardInf from "../../components/card/card"
 import { Grid } from '@mui/material'
 import { PaginationComponent } from '../../components/pagination/pagination'
 import { useSelector } from 'react-redux'
-import FiltersComponent from '../../components/filtersComponent/filtersComponent'
 const Shop = () => {
 
   const Products =  useSelector((state) =>state.productPag.products)
