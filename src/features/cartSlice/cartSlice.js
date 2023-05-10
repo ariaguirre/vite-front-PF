ab44739c8e0ed8205b0828baa55e37c815613c0a
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-05-10 10:25:21 -0500

    organizando el codigo en los helpers

--- src/features/cartSlice/cartSlice.js
+++ src/features/cartSlice/cartSlice.js
@@ -1,4 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
+import addCartItem from "../../helper/addCartItem";
+import removeCartItem from "../../helper/removeCartItem";
+import clearCartItem from "../../helper/clearCartItem";
 
 const initialState = {
   cartItems: [],
@@ -27,41 +30,6 @@ export const cartSlice = createSlice({
     }
   },
 });
-
-const addCartItem = (cartItems, productToAdd) => {
-  //find if cartItems contains productsToAdd
-  const existingCartItem = cartItems.find(
-    (cardItem) => cardItem.id === productToAdd.id
-  );
-  // if found, increment quantity
-  if (existingCartItem) {
-    return cartItems.map((cartItem) =>
-      cartItem.id === productToAdd.id
-        ? { ...cartItem, quantity: cartItem.quantity + 1 }
-        : cartItem
-    );
-  }
-  // return new array with modified cartItems / new cart Item
-  return [...cartItems, { ...productToAdd, quantity: 1 }];
-};
-const removeCartItem = (cartItems, cartItemToRemove) => {
-  const existingCartItem = cartItems.find(
-    (cartItem) => cartItem.id === cartItemToRemove.id
-  );
-  //check if quantity is equal to 1, if it is remove that item from the cart
-  if (existingCartItem.quantity === 1) {
-    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
-  }
-  // return back cartItems witch matching cart Item with reduce quantity
-  return cartItems.map((cartItem) =>
-    cartItem.id === cartItemToRemove.id
-      ? { ...cartItem, quantity: cartItem.quantity - 1 }
-      : cartItem
-  );
-};
-const clearCartItem = (cartItems, cartItemToClear) => 
-cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
-
 
 export const { addItemToCart, deleteCartItem, clearItemFromCart, setCartTotal, clearCart } = cartSlice.actions;
 
