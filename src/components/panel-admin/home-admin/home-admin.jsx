febf5826cf8c08c4302af29bbdcae7a660956a56
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-05-10 08:52:19 -0500

    agregando estilos al admin y dashboard

--- src/components/panel-admin/home-admin/home-admin.jsx
+++ src/components/panel-admin/home-admin/home-admin.jsx
@@ -1,12 +1,13 @@
+import { Style } from "@mui/icons-material"
+import styles from "../home-admin/home-admin.module.css"
 //Import Material UI
-import { Box, 
-          Typography } from '@mui/material'
+// import { Box, Typography } from '@mui/material';
 
 
 const HomeAdmin = () => {
   return (
     <div>
-    <Box component="main"
+    {/* <Box component="main"
     sx={{ width: '50%',alignContent:"center", display: 'flex',
     justifyContent: 'center', mx:"25%", mt:"2%"}}
      textAlign={"center"}
@@ -16,7 +17,52 @@ const HomeAdmin = () => {
      >
     <Typography variant="h6" color="initial" align='center'>PANEL ADMINISTRADOR</Typography>
 
-    </Box>
+    </Box> */}
+
+    <h1>Panel Principal</h1>
+    <div className={styles.container}>
+      <p className={styles.item}>Ordenes</p>
+      <div className={styles.flex}>
+        <div className={styles.areaPanel}>
+          <p className={styles.title}>Hoy</p>
+          <p className={styles.value}>2</p>
+          <p className={styles.vExtra}>Ordenes del día</p>
+        </div>
+
+        <div className={styles.areaPanel}>
+          <p className={styles.title}>Esta semana</p>
+          <p className={styles.value}>25</p>
+          <p className={styles.vExtra}>Ordenes esta semana</p>
+        </div>
+
+        <div className={styles.areaPanel}>
+          <p className={styles.title}>Este mes</p>
+          <p className={styles.value}>32</p>
+          <p className={styles.vExtra}>Ordenes en el mes</p>
+        </div>
+      </div>
+
+      <p className={styles.item}>Ingresos</p>
+      <div className={styles.flex}>
+        <div className={styles.areaPanel}>
+          <p className={styles.title}>Hoy</p>
+          <p className={styles.value}>2</p>
+          <p className={styles.vExtra}>Ordenes del día</p>
+        </div>
+
+        <div className={styles.areaPanel}>
+          <p className={styles.title}>Esta semana</p>
+          <p className={styles.value}>25</p>
+          <p className={styles.vExtra}>Ordenes esta semana</p>
+        </div>
+
+        <div className={styles.areaPanel}>
+          <p className={styles.title}>Este mes</p>
+          <p className={styles.value}>32</p>
+          <p className={styles.vExtra}>Ordenes en el mes</p>
+        </div>
+      </div>
+    </div>
    
     </div>
   )
---
+++ src/components/panel-admin/user/user.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import styles from './user.module.css';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { getUserData } from '../../../features/userData/userDataSlice';
+import { Button, Stack } from '@mui/material';
+import { getUserAdmin } from '../../../utils/firebase/firebaseClient';
+
+
+const User = () =>{
+    const dispatch = useDispatch();
+    const data = useSelector((state) =>state.currentUser.userCredentials);
+
+
+    useEffect(()=>{
+        dispatch(getUserData(getUserAdmin()))
+    },[])
+
+    console.log(data)
+
+    const img = data.reloadUserInfo.photoUrl;
+
+    return(<div className={styles.perfil} >
+        <h1>Mi perfil</h1>
+        <div >
+
+            <div className={styles.margin}>{
+                img === undefined ? <img src='https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png' className={styles.imgUsuario} alt="usr"/> : <img src={img} alt="User" className={styles.imgUsuario} />                
+            }</div>
+
+
+
+            <div className={styles.infUsrBase} >
+                <h3>Información básica</h3>
+                <p><span>Nombre:</span>{data.displayName} </p>
+                <p><span>Email:</span>{data.email} </p>
+                <Stack  alignItems='center' py={2}>
+                <Button variant='contained' size='large' className={styles.btn} >Desactivar cuenta</Button>
+            </Stack>
+            </div>
+
+        </div>
+    </div>)
+} 
+
+
+export default User;
+
