fa6d989d9b88f53b36589c0a84b4538e2e928de0
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-05-10 10:26:03 -0500

    organizando el cogido en los helpers

---
+++ src/helper/removeCartItem.js
@@ -0,0 +1,16 @@
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+  //check if quantity is equal to 1, if it is remove that item from the cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+  // return back cartItems witch matching cart Item with reduce quantity
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+export default removeCartItem;
