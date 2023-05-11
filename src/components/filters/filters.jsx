c4e20cbe12d9741128b384707a4132aea72beed7
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-05-11 16:35:19 -0500

    arreglando la estructura de carpetas de home ademas nuevos estilos y compoentes, tambien se arreglo el error en los listros de componentes neseted

--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -6,82 +6,82 @@ import { getProductsActions, prodFilterCopy, productsFilterAction, productsOrder
 import { SliderPrice } from "./slider";
 const Filters = () => {
   const dispatch = useDispatch()
-  const  {productsName,productsCopy, productsNameCopy } = useSelector(state =>state.products )
-    const categories = useSelector(state => state.categories.categories)
+  const { productsName, productsCopy, productsNameCopy } = useSelector(state => state.products)
+  const categories = useSelector(state => state.categories.categories)
   const [showMenu1, setShowMenu1] = useState(false);
   const [showMenu2, setShowMenu2] = useState(false);
   const [showMenu3, setShowMenu3] = useState(false);
   const [showMenu4, setShowMenu4] = useState(false);
   const handleClick1 = () => {
     setShowMenu1(!showMenu1);
-    if(showMenu2)setShowMenu2(!showMenu2);
+    if (showMenu2) setShowMenu2(!showMenu2);
   };
 
   const handleClick2 = () => {
     setShowMenu2(!showMenu2);
-    if(showMenu1)setShowMenu1(!showMenu1);
-  }; 
+    if (showMenu1) setShowMenu1(!showMenu1);
+  };
   const handleClick3 = () => {
     setShowMenu3(!showMenu3);
-    if(showMenu4)setShowMenu4(!showMenu4);
-  }; 
+    if (showMenu4) setShowMenu4(!showMenu4);
+  };
   const handleClick4 = () => {
     setShowMenu4(!showMenu4);
-    if(showMenu3)setShowMenu3(!showMenu3);
-  }; 
+    if (showMenu3) setShowMenu3(!showMenu3);
+  };
 
-  const handlerCategory = async (e) =>{
-   
+  const handlerCategory = async (e) => {
 
-    let filterCategoryCopy =  productsCopy.filter(doc => {
-      return doc.categories.some((a)=>{
-       return a.toLowerCase().includes(e.toLowerCase())
-         })
-      }); 
+
+    let filterCategoryCopy = productsCopy.filter(doc => {
+      return doc.categories.some((a) => {
+        return a.toLowerCase().includes(e.toLowerCase())
+      })
+    });
     dispatch(prodFilterCopy(filterCategoryCopy))
-    if(productsName.length){
-      
-      const filterCategory =  productsNameCopy.filter(doc => {
-        return doc.categories.some((a)=>{
-         return a.toLowerCase().includes(e.toLowerCase())
-           })
-        }); 
-        dispatch(getProductsActions(filterCategory))
-        dispatch(productsFilterAction(filterCategory))
+    if (productsName.length) {
+
+      const filterCategory = productsNameCopy.filter(doc => {
+        return doc.categories.some((a) => {
+          return a.toLowerCase().includes(e.toLowerCase())
+        })
+      });
+      dispatch(getProductsActions(filterCategory))
+      dispatch(productsFilterAction(filterCategory))
     }
-    else{
-    
-      const filterCategory =  productsCopy.filter(doc => {
-        return doc.categories.some((a)=>{
-         return a.toLowerCase().includes(e.toLowerCase())
-           })
-        }); 
-        dispatch(getProductsActions(filterCategory))
-        dispatch(productsFilterAction(filterCategory))
+    else {
+
+      const filterCategory = productsCopy.filter(doc => {
+        return doc.categories.some((a) => {
+          return a.toLowerCase().includes(e.toLowerCase())
+        })
+      });
+      dispatch(getProductsActions(filterCategory))
+      dispatch(productsFilterAction(filterCategory))
     }
-   
 
 
+
   }
-const noFilter = () =>{
-  if(productsName.length){
-    dispatch(getProductsActions(productsNameCopy))
-    dispatch(prodFilterCopy([]))
-    dispatch(productsFilterAction([]))
+  const noFilter = () => {
+    if (productsName.length) {
+      dispatch(getProductsActions(productsNameCopy))
+      dispatch(prodFilterCopy([]))
+      dispatch(productsFilterAction([]))
+    }
+    else {
+      dispatch(getProductsActions(productsCopy))
+      dispatch(prodFilterCopy([]))
+      dispatch(productsFilterAction([]))
+    }
   }
-  else{
-    dispatch(getProductsActions(productsCopy))
-    dispatch(prodFilterCopy([]))
-    dispatch(productsFilterAction([]))
-  }
-  }
-  const orderType = (order) =>{
+  const orderType = (order) => {
     dispatch(productsOrder(order))
   }
   return (
     <aside className={styles.filtersContainer}>
       <div className={styles.fitersTitle}>filtros</div>
-      <div className={styles.fitersTitle}></div>
+      
       <nav className={styles.nav}>
         <ul className={styles.list}>
           <li className={`${styles.listItem} ${styles.listItemClick}`}>
@@ -97,28 +97,27 @@ const noFilter = () =>{
               className={styles.listShow}
               style={{ height: showMenu1 ? "auto" : "0px" }}
             >
-                 <li>
-                <Link
-                  to="#"
-                  onClick={() =>{noFilter() }}
+              <li>
+                <span                  
+                  onClick={() => { noFilter() }}
                   className={`${styles.navLink} ${styles.navLinkInside}`}
                 >
-                 Todos
-                </Link>
+                  Todos
+                </span>
               </li>
-           { categories?categories.map((a,i) =>
-            <li   key={`${a.id}+${i}`} className={styles.listInside}>
-                <Link
-                  to="#"
-                  onClick={()=>{handlerCategory(a.categories)}}
-                  className={`${styles.navLink} ${styles.navLinkInside}`}
-                
-                >
-                  {a.categories}
-                </Link>
-              </li>
-            )  : (null)
-               }
+              {categories ? categories.map((a, i) =>
+                <li key={`${a.id}+${i}`} className={styles.listInside}>
+                  <span
+                    to="#"
+                    onClick={() => { handlerCategory(a.categories) }}
+                    className={`${styles.navLink} ${styles.navLinkInside}`}
+
+                  >
+                    {a.categories}
+                  </span>
+                </li>
+              ) : (null)
+              }
             </ul>
           </li>
 
@@ -127,121 +126,117 @@ const noFilter = () =>{
               className={`${styles.listButton} ${styles.listButtonClick}`}
               onClick={handleClick2}
             >
-              <span className={styles.navLink}>
-               Filtros
-              </span>
+              <span className={styles.navLink}>Filtros</span>
             </div>
             <ul
               className={styles.listShow}
               style={{ height: showMenu2 ? "auto" : "0px" }}
             >
-              <li className={styles.listInside}>
-                <Link
+              <ul className={styles.listInside}>
+                <span
                   to="#"
                   className={`${styles.navLink} ${styles.navLinkInside}`}
                 >
-                <li className={`${styles.listItem} ${styles.listItemClick}`}>
-            <div
-              className={`${styles.listButton} ${styles.listButtonClick}`}
-              onClick={handleClick3}
-            >
-              <span className={styles.navLink}>
-               Ordenamientos
-              </span>
-            </div>
-            <ul
-              className={styles.listShow}
-              style={{ height: showMenu3 ? "auto" : "0px" }}
-            >
-              <li className={styles.listInside}>
-                <Link
-                  to="#"
-                  className={`${styles.navLink} ${styles.navLinkInside}`}
-                >
-                <li>
-                <Link
-                  to="#"
-                  onClick={() =>{orderType({orderBy :"name", orderType : "asc"}) }}
-                  className={`${styles.navLink} ${styles.navLinkInside}`}
-                >
-                 Nombre A - Z 
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="#"
-                  onClick={() =>{orderType({orderBy :"name", orderType : "desc"}) }}
-                  className={`${styles.navLink} ${styles.navLinkInside}`}
-                >
-                 Nombre Z - A 
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="#"
-                  onClick={() =>{orderType({orderBy :"rating", orderType : "desc"}) }}
-                  className={`${styles.navLink} ${styles.navLinkInside}`}
-                >
-                 Mas rating
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="#"
-                  onClick={() =>{orderType({orderBy :"rating", orderType : "asc"}) }}
-                  className={`${styles.navLink} ${styles.navLinkInside}`}
-                >
-                 Menos rating
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="#"
-                  onClick={() =>{orderType({orderBy :"price", orderType : "desc"}) }}
-                  className={`${styles.navLink} ${styles.navLinkInside}`}
-                >
-                 Mayor precio
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="#"
-                  onClick={() =>{orderType({orderBy :"price", orderType : "asc"}) }}
-                  className={`${styles.navLink} ${styles.navLinkInside}`}
-                >
-                 Menor precio
-                </Link>
-              </li>
-                </Link>
-              </li>
-            </ul>
-          </li>
-                <li className={`${styles.listItem} ${styles.listItemClick}`}>
-            <div
-              className={`${styles.listButton} ${styles.listButtonClick}`}
-              onClick={handleClick4}
-            >
-              <span className={styles.navLink}>
-               Rango de precios
-              </span>
-            </div>
-            <ul
-              className={styles.listShow}
-              style={{ height: showMenu4 ? "auto" : "0px" }}
-            >
-              <li className={styles.listInside}>
-                <Link
-                  to="#"
-                  className={`${styles.navLink} ${styles.navLinkInside}`}
-                >
-     <SliderPrice/>
-                </Link>
-              </li>
-            </ul>
-          </li>
-                </Link>
-              </li>
-              
+                  <li className={`${styles.listItem} ${styles.listItemClick}`}>
+                    <div
+                      className={`${styles.listButton} ${styles.listButtonClick}`}
+                      onClick={handleClick3}
+                    >
+                      <span className={styles.navLink}>
+                        Ordenamientos
+                      </span>
+                    </div>
+                    <ul
+                      className={styles.listShow}
+                      style={{ height: showMenu3 ? "auto" : "0px" }}
+                    >
+                      <ul className={styles.listInside}>
+                        <span                          
+                          className={`${styles.navLink} ${styles.navLinkInside}`}
+                        >
+                          <li>
+                            <Link
+                              to="#"
+                              onClick={() => { orderType({ orderBy: "name", orderType: "asc" }) }}
+                              className={`${styles.navLink} ${styles.navLinkInside}`}
+                            >
+                              Nombre A - Z
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              to="#"
+                              onClick={() => { orderType({ orderBy: "name", orderType: "desc" }) }}
+                              className={`${styles.navLink} ${styles.navLinkInside}`}
+                            >
+                              Nombre Z - A
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              to="#"
+                              onClick={() => { orderType({ orderBy: "rating", orderType: "desc" }) }}
+                              className={`${styles.navLink} ${styles.navLinkInside}`}
+                            >
+                              Mas rating
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              to="#"
+                              onClick={() => { orderType({ orderBy: "rating", orderType: "asc" }) }}
+                              className={`${styles.navLink} ${styles.navLinkInside}`}
+                            >
+                              Menos rating
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              to="#"
+                              onClick={() => { orderType({ orderBy: "price", orderType: "desc" }) }}
+                              className={`${styles.navLink} ${styles.navLinkInside}`}
+                            >
+                              Mayor precio
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              to="#"
+                              onClick={() => { orderType({ orderBy: "price", orderType: "asc" }) }}
+                              className={`${styles.navLink} ${styles.navLinkInside}`}
+                            >
+                              Menor precio
+                            </Link>
+                          </li>
+                        </span>
+                      </ul>
+                    </ul>
+                  </li>
+                  <ul className={`${styles.listItem} ${styles.listItemClick}`}>
+                    <div
+                      className={`${styles.listButton} ${styles.listButtonClick}`}
+                      onClick={handleClick4}
+                    >
+                      <span className={styles.navLink}>
+                        Rango de precios
+                      </span>
+                    </div>
+                    <ul
+                      className={styles.listShow}
+                      style={{ height: showMenu4 ? "auto" : "0px" }}
+                    >
+                      <li className={styles.listInside}>
+                        <span                        
+                          className={`${styles.navLink} ${styles.navLinkInside}`}
+                        >
+                          <SliderPrice />
+                        </span>
+                      </li>
+                    </ul>
+                  </ul>
+                </span>
+              </ul>
+
             </ul>
           </li>
         </ul>
--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,8 +1,16 @@
-import Hero  from "../../components/hero-section/hero"
+import HeroComponent from "../../components/hero-components/hero-component/hero-component"
+import HomeCarousel from "../../components/hero-components/home-carousel/home-carousel";
+import ProductsByRaiting from "../../components/hero-components/products-by-raiting/products-by-raiting";
+import ProductsBySale from "../../components/hero-components/products-by-sale/products-by-sale";
 
 const Home = () => {
-return (             
-        <Hero/>            
+  return (
+    <>
+      <HeroComponent />
+      <ProductsByRaiting />
+      <HomeCarousel />
+      <ProductsBySale />
+    </>
   )
 }
 
