c702a1985cd020efcbb7ee8e75b90eaec8876402
Ariana <ariaguirre64@gmail.com>
2023-05-23 15:17:42 -0300

    emailjs agregado

--- src/routes/contactForm/contactForm.jsx
+++ src/routes/contactForm/contactForm.jsx
@@ -1,9 +1,42 @@
 import style from "./contactForm.module.css"
 import TextField from '@mui/material/TextField'
 import Stack from '@mui/material/Stack'
+import {useState} from 'react'
+import emailjs from '@emailjs/browser';
+import Swal from "sweetalert2";
+
+
 
 
 const ContactForm = () => {
+
+    //EmailJs
+    const service = 'service_82h1k7l';
+    const template = 'template_lgs2fiu';
+    const apiKey = 'oqauhqfeYX6gN7rRJ';
+
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [issue, setIssue] = useState('')
+    const [text, setText] = useState('')
+
+    console.log(params)
+    const sendEmail = () => {
+        const params = {
+            email: email,
+            fromName: name,
+            issue: issue,
+            message: text
+        }
+        email.send(service, template, params, apiKey).then(
+            Swal.fire({
+                title: 'Correo enviado!',
+                text: 'Pronto responderemos tu mensaje.',
+                icon: 'success',
+              })
+        )
+    }
 
     return(
         <div>
@@ -19,18 +52,24 @@ const ContactForm = () => {
           type="email"
           autoComplete=""
           required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <TextField
           label="Nombre"
           type="text"
           autoComplete=""
           required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <TextField
           label="Asunto"
           type="text"
           autoComplete=""
           required
+          value={issue}
+          onChange={(e) => setIssue(e.target.value)}
         />
         <TextField
           label="Consulta, sugerencia o comentario"
@@ -38,11 +77,14 @@ const ContactForm = () => {
           autoComplete=""
           required
           size="large"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
           className={style.consulta}
           sx = {{
             marginBottom: '60%'
           }}
         />
+          <button type="submit" className={style.btn}>Enviar</button>
       </Stack>
         </div>
     )
